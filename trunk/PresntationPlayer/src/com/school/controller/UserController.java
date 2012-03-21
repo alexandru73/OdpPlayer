c8d1ca6dccef22bd965990ee36f932bac55c99b9
alexandru73 <alex.ilea73@yahoo.com>
2012-03-21 18:32:48 +0200

    working chain

--- trunk/PresntationPlayer/src/com/school/controller/UserController.java
+++ trunk/PresntationPlayer/src/com/school/controller/UserController.java
@@ -1,17 +1,25 @@
 package com.school.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.school.converter.Converter;
+import com.school.converter.ConverterContext;
+
 @Controller
 public class UserController {
-
+	
+	@Resource(name="presentationConverter")
+	Converter converter;
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/int", headers = "Accept=application/json")
 	public @ResponseBody
 	String getAllEmp() {
-		System.out.println("mess");
+		converter.convert(new ConverterContext());
 		return "god";
 	}
 
--- trunk/PresntationPlayer/src/com/school/model/User.java
+++ trunk/PresntationPlayer/src/com/school/model/User.java
@@ -1,8 +1,31 @@
 package com.school.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class User {
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+	
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
