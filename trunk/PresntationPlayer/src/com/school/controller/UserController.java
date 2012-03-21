3ee596e1a5afd2a1a1c9cbc8734d612b27b40939
alexandru73 <alex.ilea73@yahoo.com>
2012-03-21 21:04:16 +0200

    #config added

--- trunk/PresntationPlayer/src/com/school/controller/UserController.java
+++ trunk/PresntationPlayer/src/com/school/controller/UserController.java
@@ -1,24 +1,30 @@
 package com.school.controller;
 
+import java.util.Locale;
+
 import javax.annotation.Resource;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.school.config.ConfigurationLoader;
 import com.school.converter.Converter;
 import com.school.converter.ConverterContext;
 
 @Controller
 public class UserController {
-	
-	@Resource(name="presentationConverter")
+	@Resource(name = "configLoader")
+	ConfigurationLoader resources;
+	@Resource(name = "presentationConverter")
 	Converter converter;
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/int", headers = "Accept=application/json")
 	public @ResponseBody
 	String getAllEmp() {
+		System.out.println("zuuz" + resources.getConfig().getString("local.repository.upload.path"));
 		converter.convert(new ConverterContext());
 		return "god";
 	}
