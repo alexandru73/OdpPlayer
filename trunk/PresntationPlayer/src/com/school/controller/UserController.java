01a788cfe3eb25642b6a9b7827fe570659ea58fb
alexandru73 <alex.ilea73@yahoo.com>
2012-03-24 01:30:37 +0200

    #-port number set

--- trunk/PresntationPlayer/src/com/school/controller/UserController.java
+++ trunk/PresntationPlayer/src/com/school/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
 	public @ResponseBody
 	String getAllEmp() {
 		System.out.println("zuuz" + resources.getConfig().getString("local.repository.upload.path"));
-		for (long i = 200; i < 250; i++) {
+		for (long i = 2; i < 250; i++) {
 			queue.send(new Job(i));
 		}
 		
--- trunk/PresntationPlayer/src/com/school/util/Config.java
+++
@@ -1,5 +0,0 @@
-package com.school.util;
-
-public class Config {
-
-}
